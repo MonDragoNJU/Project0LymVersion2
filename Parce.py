69e972dda86c041c647c87b8a0fb476bc4378454
MonDragoNJU <j.mondragon1@uniandes.edu.co>
2023-09-02 13:32:54 -0500

    ko

--- Parce.py
+++ Parce.py
@@ -4,12 +4,16 @@ def read_file(file_name: str):
     
     file = open(file_name, "r", encoding="utf-8")
     
-    line = file.read()
+    line = file.readline()
     line = line.replace("\n", " ")
     
     line = line.lower()
     
     file.close()
+    
+    return line
+
+print(read_file("LyM\Test.txt"))
     
     
     
