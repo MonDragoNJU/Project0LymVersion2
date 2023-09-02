9d79e3235a5423bd44b72cbdd2018d509bb79eb3
MonDragoNJU <j.mondragon1@uniandes.edu.co>
2023-09-02 01:02:51 -0500

    Primeros cambios

--- View.py
+++ View.py
@@ -1,1 +1,8 @@
-
+def main():
+    
+    print("Welcome to the syntax checker of the robot program.")
+    print()
+    
+    file = input("Insert the path of the file you want to read: ")
+    
+    
