adb31da212d647249e69486a70dab350eee8eddb
AnaHernandez20 <am.hernandezl1@uniandes.edu.co>
2023-09-03 11:14:13 -0500

    Analize_commands

--- Rules.py
+++ Rules.py
@@ -49,9 +49,21 @@ def analize_condictional(tokened_phrase: list):
     pass
 
 def analize_commands(tokened_phrase: list):
-    if tokened_phrase[0] == jump:
-        
-        
- 
-    
-    
+
+    checker_bool = True
+
+    if tokened_phrase[0] == "JUMP":
+        if tokened_phrase[1] != "LEFTPAR" or (tokened_phrase[2] != "NUM" and tokened_phrase[2] != "VAR") or tokened_phrase[3] != "COMMA" or (tokened_phrase[4] != "NUM" and tokened_phrase[4] != "VAR") or tokened_phrase[5] != "RIGHTPAR":
+            checker_bool = False
+
+    elif tokened_phrase[0] == "WALK":
+        if len(tokened_phrase) == 4:
+            if tokened_phrase[1] != "LEFTPAR" or (tokened_phrase[2] != "NUM" and tokened_phrase[2] != "VAR") or tokened_phrase[3] != "RIGHTPAR":
+                checker_bool = False
+        elif len(tokened_phrase) == 6:
+            if tokened_phrase[1] != "LEFTPAR" or (tokened_phrase[2] != "NUM" and tokened_phrase[2] != "VAR") or tokened_phrase[3] != "COMMA" or (tokened_phrase[4] != "DIRECTION" and tokened_phrase[4] != "ORIENTATION") or tokened_phrase[5] != "RIGHTPAR":
+                checker_bool = False
+        else:
+            checker_bool = False
+
+    #elif tokened_phrase[0] == "LEAP":
